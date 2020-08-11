44ec2eeb1394e3575e9f21cc0a35e3a40a923529
SilverBadger <42nd.badger@gmail.com>
2020-08-11 15:13:41 +0200

    "Tokenizing for HelloWorld"

---
+++ src/tokenizer.rs
@@ -0,0 +1,109 @@
+use regex_lexer::{LexerBuilder, Lexer};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token<'a> {
+    Data(DataToken),
+    //Operator(OperatorToken),
+    //Control(ControlToken),
+    Identifier(&'a str),
+    //Null,
+    Bracket(BracketToken),
+}
+
+impl From<DataToken> for Token<'_> {
+    fn from(data: DataToken)->Self{
+        Token::Data(data)
+    }
+}
+
+impl From<BracketToken> for Token<'_> {
+    fn from(bracket: BracketToken)->Self{
+        Token::Bracket(bracket)
+    }
+}
+
+
+#[derive(Debug, PartialEq)]
+pub enum DataToken {
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Char(char),
+    Str(String),
+}
+impl std::cmp::Eq for DataToken{
+
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum BracketToken {
+    RoundOpen,
+    RoundClose,
+    /*
+    SquareOpen,
+    SquareClose,
+    CurlyOpen,
+    CurlyClose,
+    */
+}
+/*
+pub enum OperatorToken {
+    Puls,
+    Minus,
+    Astrist,
+    Slash,
+    Percent,
+}
+
+
+
+pub enum ControlToken {
+    If,
+    Else,
+    Elif,
+    While,
+}*/
+
+pub fn build_lexer<'t>() -> Result<Lexer<'t, Token<'t>>, regex::Error> {
+LexerBuilder::new()
+
+.token(r"(true|false)", |tok| Some(DataToken::Bool(tok.parse().unwrap()).into()))
+.token(r"-?[0-9]+", |tok| Some(DataToken::Int(tok.parse().unwrap()).into()))
+.token(r"-?[0-9]+.[0-9]+", |tok| Some(DataToken::Float(tok.parse().unwrap()).into()))
+.token(r"'.'", |tok| Some(DataToken::Char(tok.parse().unwrap()).into()))
+.token("\".*\"", |tok| Some(DataToken::Str(tok[1..tok.len() - 1].parse().unwrap()).into()))
+
+.token(r"\(", |_| Some(BracketToken::RoundOpen.into()))
+.token(r"\)", |_| Some(BracketToken::RoundClose.into()))
+
+.token(r"(_|[a-zA-Z])[a-zA-Z_0-9]*", |tok| Some(Token::Identifier(tok)))
+
+/*
+.token(r"\+", |tok| Some(Token::Operator::Puls(tok.parse().unwrap()))
+.token(r"-", |tok| Some(Token::Operator::Minus(tok.parse().unwrap()))
+.token(r"\*", |tok| Some(Token::Operator::Atrist(tok.parse().unwrap()))
+.token(r"/", |tok| Some(Token::Operator::Slash(tok.parse().unwrap()))
+.token(r"%" |tok| Some(Token::Operator::(tok.parse().unwrap()))
+*/
+
+.token(r"\s", |_| None)
+
+.build()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello_world(){
+            let source = "print(\"Hello, World!\") ";
+            assert_eq!(
+                build_lexer().unwrap().tokens(source).collect::<Vec<_>>(),
+                vec![
+                    Token::Identifier("print"), BracketToken::RoundOpen.into(), DataToken::Str("Hello, World!".to_string()).into(), BracketToken::RoundClose.into()
+                ],
+            )
+    }
+}
