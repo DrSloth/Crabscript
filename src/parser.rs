5e6e6ecd4deddf05dd44a60f53569fc04fbb64bd
SilverBadger <42nd.badger@gmail.com>
2020-08-12 12:07:29 +0200

    "Parsing for functions and parentheses"

--- src/parser.rs
+++ src/parser.rs
@@ -1,33 +1,90 @@
-use regex_lexer::Tokens;
-use crate::tokenizer::{Token, DataToken, BracketToken};
 use crate::base::DayObject;
+use crate::tokenizer::{BracketToken, DataToken, Token};
+use regex_lexer::Tokens;
 
-enum Node<'a>{
+enum Node<'a> {
     Data(DayObject),
-    FunctionCall{id: &'a str,  args: Vec<Box<dyn ExpressionNode>>},
+    FunctionCall {
+        parsed: bool,
+        id: &'a str,
+        args: Vec<Node<'a>>,
+    },
     Identifier(&'a str),
+    Parentheses {
+        parsed: bool,
+        content: Vec<Node<'a>>,
+    },
 }
 
 fn parse(tokens: Tokens<Token>) {
-    let stack: Vec<Node>;
+    let mut stack: Vec<Node> = vec![];
     for token in tokens {
-        match token{
-            Token::Data(val) => stack.push(Node::Data(
-                match val {
+        match token {
+            Token::Data(val) => stack.push(Node::Data(match val {
                 DataToken::Integer(i) => DayObject::Integer(i),
                 DataToken::Float(f) => DayObject::Float(f),
                 DataToken::Bool(b) => DayObject::Bool(b),
                 DataToken::Character(c) => DayObject::Character(c),
                 DataToken::Str(s) => DayObject::Str(s),
             })),
-            Token::Identifier(id) => stack.pushNode(Node::Identifier(id)),
-            
-            Token::Bracket(BracketToken::RoundOpen) => {
-                if stack.get(stack.len - 1).is::<Identifier>(){
-                    let name = (*stack.pop().unwrap()).downcast_ref::<Identifier>().name;
-                    stack.push(Box::new(FunctionCallNode::new(name)));
+
+            Token::Identifier(id) => stack.push(Node::Identifier(id)),
+
+            Token::Bracket(BracketToken::RoundOpen) => match stack.get(stack.len() - 1) {
+                Some(Node::Identifier(id)) => {
+                    // If an identifier was before the ( it is part of a function call
+                    stack.push(Node::FunctionCall {
+                        parsed: false,
+                        id: id,
+                        args: vec![],
+                    });
+                    stack.pop();
                 }
-            }
+                _ => stack.push(Node::Parentheses {
+                    // Else its just part of regural parentheses
+                    parsed: false,
+                    content: vec![],
+                }),
+            },
+            Token::Bracket(BracketToken::RoundClose) => loop {
+                let mut content: Vec<Node> = Vec::new();
+                let top: Node = stack.pop().expect("`)` umatched!");
+
+                match top {
+                    // Top is the parentheses that are colsed by the Token::RoundClosed:
+                    Node::Parentheses { 
+                        parsed: false,
+                        content: mut previous_content,
+                    } => {
+                        content.append(&mut previous_content);
+                        stack.push(Node::Parentheses {
+                            parsed: true,
+                            content: content,
+                        });
+                        break;
+                    }
+
+                    // Top is the function cll that are colsed by the Token::RoundClosed:    
+                    Node::FunctionCall {
+                        parsed: false,
+                        id,
+                        args: mut previous_conent,
+                    } => {
+                        content.append(&mut previous_conent);
+                        stack.push(Node::FunctionCall {
+                            parsed: true,
+                            id,
+                            args: content,
+                        });
+                        break;
+                    }
+
+                    // Top is just content inside the parentheses:
+                    _ => content.push(top),
+                }
+            },
+
+            _ => todo!(),
         }
     }
 }
