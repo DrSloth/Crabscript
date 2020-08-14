b2d89c6c29bd008f95cefdec9affb53e4c5f53c6
SilverBadger <42nd.badger@gmail.com>
2020-08-14 19:47:01 +0200

    Calling off all functions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use one_day::{tokenizer, parser};
 
 fn main() {
     let lexer = tokenizer::build_lexer().unwrap();
-    let tokens = lexer.tokens("print(\"Hello, World!\") ");
+    let tokens = lexer.tokens("print(add(4 5))");
     let nodes = parser::parse(tokens);
     dbg!(&nodes);
     for n in nodes {
